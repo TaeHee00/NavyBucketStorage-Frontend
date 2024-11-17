7758cd62bf334dfcad3550a9cb8bd406dc729079
Tae Hee Jang <kancth03@gmail.com>
2024-11-18 00:03:41 +0900

    fix: 전역 상태관리 가능하도록 변경

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import React from 'react';
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<LoginPage />} />
-      </Routes>
-    </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+              <Routes>
+                  <Route path='/' element={<LoginPage />} />
+              </Routes>
+          </BrowserRouter>
+          <ReactQueryDevtools />
+      </QueryClientProvider>
   );
 }
 
