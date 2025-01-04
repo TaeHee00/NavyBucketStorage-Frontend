053403ae8c0cdbef6b79d6c5f07b2c5e2a021886
Tae Hee Jang <kancth03@gmail.com>
2025-01-04 17:32:55 +0900

    feat: file delete API 함수 구현

--- src/services/bucket/BucketAPI.ts
+++ src/services/bucket/BucketAPI.ts
@@ -26,4 +26,19 @@ export const bucketFileListAPI = async (accessToken: string, bucketId: number)=>
 
         return {status, data};
     })
-}
+};
+
+export const bucketFileDeleteAPI = async (accessToken: string, fileId: number)=> {
+    return await fetch(`http://localhost:8080/api/v1/files/${fileId}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+            "Authorization": `${accessToken}`,
+        }
+    }).then(async res => {
+        if (res.status !== 204) {
+            alert("권한 오류 발생");
+        }
+        return res.status;
+    })
+};
