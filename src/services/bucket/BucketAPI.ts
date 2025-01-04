5849c05126555f969400021d4efb29ada6372228
Tae Hee Jang <kancth03@gmail.com>
2025-01-04 16:22:32 +0900

    feat: Bucket Files API 호출 함수 구현

--- src/services/bucket/BucketAPI.ts
+++ src/services/bucket/BucketAPI.ts
@@ -12,3 +12,18 @@ export const bucketListAPI = async (accessToken: string) => {
         return {status, data};
     })
 };
+
+export const bucketFileListAPI = async (accessToken: string, bucketId: number)=> {
+    return await fetch(`http://localhost:8080/api/v1/buckets/${bucketId}`,{
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+            "Authorization": `${accessToken}`,
+        }
+    }).then(async res => {
+        const status = res.status;
+        const data = await res.json();
+
+        return {status, data};
+    })
+}
